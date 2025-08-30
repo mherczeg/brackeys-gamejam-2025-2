6023cd12c1a1a63d7dafc08974f634ed7d036573
mherczeg <herczeg.mt@gmail.com>
2025-08-31 01:34:17 +0200

    WIP resources

--- app/tools/ScriptRunner.gd
+++ app/tools/ScriptRunner.gd
@@ -3,4 +3,7 @@ extends EditorScript
 
 func _run():
 	var generator = TresGenerator.new()
-	generator.generate("res://data/my_resources.json", "res://resources/generated")
+	# generator.generate("res://data/bases.json", "res://resources/generated/bases")
+	# generator.generate("res://data/effects.json", "res://resources/generated/effects")
+	# generator.generate("res://data/ingredients.json", "res://resources/generated/ingredients")
+	generator.generate("res://data/products.json", "res://resources/generated/products")
